f8c2817580b51681ee34deb4bda66cbf41783929
serqol <serqol@mail.ru>
2018-02-23 17:46:58 +0300

    das

---
+++ tests/test.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests;
+use PhpOffice\PhpWord\IOFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$fileName = __DIR__ . '/1Воронеж Заключение.doc';
+$phpWord = IOFactory::load($fileName, 'MsDoc');
+
+$sections = $phpWord->getSections();
+$section = $sections[0];
+$arrays = $section->getElements();
+
+var_dump($arrays);
